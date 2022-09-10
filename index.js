1aaeb4e0cdd37f01d2f636f64ac21a0c47037949
rivopelu <rivopelu12@gmail.com>
2022-09-10 10:07:48 +0700

    [RIVO][1]modified

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 
 
+app.get("/", function (req, res) {
+  res.send("welcome")
+})
+
 app.listen(8080, () => {
   console.log("server telah berjalan")
 })
